39d3ffc2b5d60924d7654c67d81b36c866e4afaf
Jessica <jessica.herrera82094@gmail.com>
2019-02-14 18:40:28 -0800

    added server dependencies

---
+++ client/src/index.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+ 
+class Zillow extends React.Component {
+  render() {
+    return <div>Hello World</div>;
+  }
+}
+ 
+ReactDOM.render(<Zillow />, document.getElementById('app'));
---
+++ server/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/../client/dist'));
+
+var port = 3002;
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+ 
+app.listen(port, ()=>{
+  console.log(`Listening on Port: ${port}`);
+});
